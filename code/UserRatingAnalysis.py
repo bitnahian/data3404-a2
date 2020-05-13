c48f0fb7393c0943a13f757c28ae924968d653bb
bitnahian <nhas9102@uni.sydney.edu.au>
2020-05-14 05:45:58 +1000

    Pls work this time man

--- code/UserRatingAnalysis.py
+++ code/UserRatingAnalysis.py
@@ -2,14 +2,14 @@ from pyspark.sql import SparkSession
 from pyspark.sql import functions as F
 from operator import add
 import re
-​
+
 # filter for at least rating 5;
 # group by userid count 5 star ratings per user
 # join with users table for firstname and last name
 def userratinganalysis(spark):
     comments = spark.read.csv('s3://data3404-nhas9102-a2/data/comments.csv', inferSchema =True, header=True)\
                          .select('to_user_id', 'rating')
-​
+    
     comments = comments.filter(comments.rating >= 5)
     comments = comments.drop('rating')
     comment_rows = comments.rdd.map(lambda x: x[0])
@@ -17,16 +17,13 @@ def userratinganalysis(spark):
                          .reduceByKey(add)
     counts_sorted = counts.sortBy(lambda x: x[1], False)
     output = counts_sorted.collect()
-​
+    
     for (user, count) in output:
         print("%s: %i" % (user, count))
-​
-​
+
 if __name__ == "__main__":
-​
     spark = SparkSession\
             .builder.appName('UserRatingAnalysis')\
             .getOrCreate()
-​
     userratinganalysis(spark)
     spark.stop()
