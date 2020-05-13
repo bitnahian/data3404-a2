4caf42374527d3eeefe8dd39f798bbadec69db8b
bitnahian <nhas9102@uni.sydney.edu.au>
2020-05-14 05:39:21 +1000

    Pls pls pls work

--- code/UserRatingAnalysis.py
+++ code/UserRatingAnalysis.py
@@ -2,32 +2,31 @@ from pyspark.sql import SparkSession
 from pyspark.sql import functions as F
 from operator import add
 import re
-
+​
 # filter for at least rating 5;
 # group by userid count 5 star ratings per user
 # join with users table for firstname and last name
 def userratinganalysis(spark):
-    comments = spark.read.csv("s3://data3404-nhas9102-a2/data/comments.csv", inferSchema =True, header=True)\
-                         .select('to_user_id', 'rating') # DataFrame 
-
+    comments = spark.read.csv('s3://data3404-nhas9102-a2/data/comments.csv', inferSchema =True, header=True)\
+                         .select('to_user_id', 'rating')
+​
     comments = comments.filter(comments.rating >= 5)
-    comments = comments.drop('rating') # -> 'to_user_id' -> [.., ..., ...]
-    comment_rows = comments.rdd.map(lambda x: x[0]) # -> [ id1, id2, id3, ...] 
-    counts = comment_rows.map(lambda x: (x, 1)) \ 
-                         .reduceByKey(add) # -> [ (id1, X), (id2, Y), (id3, Z),...]
+    comments = comments.drop('rating')
+    comment_rows = comments.rdd.map(lambda x: x[0])
+    counts = comment_rows.map(lambda x: (x, 1)) \
+                         .reduceByKey(add)
     counts_sorted = counts.sortBy(lambda x: x[1], False)
     output = counts_sorted.collect()
-
+​
     for (user, count) in output:
         print("%s: %i" % (user, count))
-
-
+​
+​
 if __name__ == "__main__":
-
+​
     spark = SparkSession\
             .builder.appName('UserRatingAnalysis')\
             .getOrCreate()
-
+​
     userratinganalysis(spark)
     spark.stop()
-    # BLAH # FOO
